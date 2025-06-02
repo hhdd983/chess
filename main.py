77e152cf5677a1ea23dbd695cc37d80a22fe90f1
Boaztheprogrammer <120393293+Boaztheprogrammer@users.noreply.github.com>
2025-06-02 11:05:13 +0900

    Update main.py
    added ai from chess ai library

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import pygame
 import chess
+import chess.engine
 
 # Setting
 pygame.init()
-width, height = 560, 560 #560 *560
-square_size = width // 8 #square_size 70
-white = (238, 238, 210) #white color
-black = (118, 150, 86)  #black color
+width, height = 560, 560  # 560 *560
+square_size = width // 8  # square_size 70
+white = (238, 238, 210)  # white color
+black = (118, 150, 86)  # black color
 text_color = (200, 0, 0)
 
 font = pygame.font.SysFont(None, 48)
@@ -18,25 +19,35 @@ clock = pygame.time.Clock()
 # White pieces (P,N,B.R.Q.K) black pieces (p,n,b,r,q,k)
 piece_images = {}
 
-def parser(s): # If the first letter start with 'w', change it to capital letter else small letter
+
+def parser(s):  # If the first letter start with 'w', change it to capital letter else small letter
     if s[0] == 'w':
         return chr(ord(s[1])-32)
     else:
         return s[1]
 
-for symbol in ['wp','wn','wb','wr','wq','wk','bp','bn','bb','br','bq','bk']:
-    img = pygame.image.load(f'{symbol}.png') 
-    piece_images[parser(symbol)] = pygame.transform.scale(img, (square_size, square_size))
+
+for symbol in ['wp', 'wn', 'wb', 'wr', 'wq', 'wk', 'bp', 'bn', 'bb', 'br', 'bq', 'bk']:
+    img = pygame.image.load(f'{symbol}.png')
+    piece_images[parser(symbol)] = pygame.transform.scale(
+        img, (square_size, square_size))
 # lamba is the one that we can use without making any values
+
+# chess engine initialization
+engine = chess.engine.SimpleEngine.popen_uci(
+    r"/opt/homebrew/Cellar/stockfish/17.1/bin/stockfish")
 
 # python-chess Create board
 board = chess.Board()
 
 # UCI = now/to move (e2e4)
 files = 'abcdefgh'
+
+
 def coord_to_uci(col, row):
 
-    return files[col] + str(8 - row) # col = vertical line, row = horizontal line 
+    # col = vertical line, row = horizontal line
+    return files[col] + str(8 - row)
 # (8 - row) to reverse the board
 
 
@@ -58,7 +69,7 @@ while running:
             uci_sq = coord_to_uci(col, row)
             idx = chess.square(col, 7 - row)
             piece = board.piece_at(idx)
-            if piece and piece.color == board.turn: # Can only click when it's your turn
+            if piece and piece.color == board.turn:  # Can only click when it's your turn
                 selected_sq = uci_sq
 
         # Mouse unclick -> ending point / if not possible it doesn't move
@@ -77,15 +88,17 @@ while running:
             # 2) If it's a right point -> play
             if move and move in board.legal_moves:
                 board.push(move)
-
+                result = engine.play(board, chess.engine.Limit(time=0.1))
+                board.push(result.move)
 
     # Print chess board
     for r in range(8):
         for c in range(8):
-            color = white if (r + c) % 2 == 0 else black # If it's even number white else black ((r + c) % 2 == 0)
+            # If it's even number white else black ((r + c) % 2 == 0)
+            color = white if (r + c) % 2 == 0 else black
             rect = (c * square_size, r * square_size, square_size, square_size)
             pygame.draw.rect(screen, color, rect)
-    rect = (560,0,280,560)
+    rect = (560, 0, 280, 560)
     pygame.draw.rect(screen, black, rect)
 
     # Pieces
@@ -110,7 +123,8 @@ while running:
         text_surf = font.render(msg, True, text_color)
         text_rect = text_surf.get_rect(center=(width // 2, height // 2))
         # 반투명 배경 박스
-        overlay = pygame.Surface((text_rect.width + 20, text_rect.height + 20), pygame.SRCALPHA)
+        overlay = pygame.Surface(
+            (text_rect.width + 20, text_rect.height + 20), pygame.SRCALPHA)
         overlay.fill((255, 255, 255, 180))
         screen.blit(overlay, (text_rect.x - 10, text_rect.y - 10))
         screen.blit(text_surf, text_rect)
